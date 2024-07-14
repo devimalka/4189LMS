d8bf5afc36672a151baf0960015cab0cd28d4a57
devim <developerimalka@gmail.com>
2024-07-14 20:22:26 +0530

    Add tailwindcss and vite for frontend

--- server/server.js
+++ server/server.js
@@ -1,12 +1,9 @@
-const express = require('express');
-const app = express();
-const port = 3000
-
-app.get('/',(req,res) => {
-  res.send("Hello world")
-})
-
-
-app.listen(port,() => {
-  console.log(`port listening on ${port}`);
-})
+var express = require('express');
+var app = express();
+var port = 3000;
+app.get('/', function (req, res) {
+    res.send("Hello world");
+});
+app.listen(port, function () {
+    console.log("port listening on ".concat(port));
+});
